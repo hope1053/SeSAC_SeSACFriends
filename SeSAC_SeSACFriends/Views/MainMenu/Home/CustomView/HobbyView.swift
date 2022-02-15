46309b14984f3d7cce1682ab64cdbb127eaa6988
hyerin <hope1053@naver.com>
2022-02-15 13:50:09 +0900

    Fix: HobbyView HeaderView 우측정렬되는 문제 해결

--- SeSAC_SeSACFriends/Views/MainMenu/Home/CustomView/HobbyView.swift
+++ SeSAC_SeSACFriends/Views/MainMenu/Home/CustomView/HobbyView.swift
@@ -10,9 +10,12 @@ import UIKit
 class HobbyView: UIView, BaseView {
     
     let collectionView: UICollectionView = {
-        let layout = CollectionViewLeftAlignFlowLayout()
+        let layout = LeftAlignedCollectionViewFlowLayout()
+//        layout.headerReferenceSize = CGSize(width: UIScreen.main.bounds.width, height: 18)
         layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
         layout.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+        layout.minimumLineSpacing = 8
+        layout.minimumInteritemSpacing = 8
         
         let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
         
@@ -56,24 +59,24 @@ class HobbyView: UIView, BaseView {
     }
 }
 
-class CollectionViewLeftAlignFlowLayout: UICollectionViewFlowLayout {
-    let cellSpacing: CGFloat = 8
- 
+final class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
+    
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        self.minimumLineSpacing = 8
-        self.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
-        let attributes = super.layoutAttributesForElements(in: rect)
- 
-        var leftMargin = sectionInset.left
-        var maxY: CGFloat = -1.0
-        attributes?.forEach { layoutAttribute in
-            if layoutAttribute.frame.origin.y >= maxY {
-                leftMargin = sectionInset.left
-            }
-            layoutAttribute.frame.origin.x = leftMargin
-            leftMargin += layoutAttribute.frame.width + cellSpacing
-            maxY = max(layoutAttribute.frame.maxY, maxY)
-        }
-        return attributes
-    }
+     
+     let attributes = super.layoutAttributesForElements(in: rect)
+     var leftMargin = sectionInset.left
+     var maxY: CGFloat = -1.0
+     
+     attributes?.forEach { layoutAttribute in
+         if layoutAttribute.representedElementCategory == .cell {
+             if layoutAttribute.frame.origin.y >= maxY {
+               leftMargin = sectionInset.left
+             }
+             layoutAttribute.frame.origin.x = leftMargin
+             leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
+             maxY = max(layoutAttribute.frame.maxY, maxY)
+         }
+     }
+     return attributes
+ }
 }
